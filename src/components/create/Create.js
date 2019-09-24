341c6db007a0e72c3ee806a93bf50bcac7d6afe7
ricardo <ricardo.sikic@gmail.com>
2019-09-24 16:05:06 -0300

    agregada funcionalidad de creacion solo falta mejorarla e implementarla como vista pasandola por props

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -1,9 +1,54 @@
 import React, { Fragment } from 'react';
 
 export class CreatePost extends React.Component {
+    
+    state = {
+        newPost: []
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        let newObj = { [name]: value };
+        const newData = {...this.state.newPost, ...newObj};
+        this.setState({
+            newPost: newData
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('posted ', this.state.newPost);
+        const secretKey = localStorage.getItem('key');
+        let url = 'http://127.0.0.1:8000/api/';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(this.state.newPost),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: `token ${secretKey}`
+            }
+        })
+        .then(resp => {
+            console.log('respuesta de la subida ', resp.status);
+            return resp.text();
+        })
+    }
+   
+
+
     render() {
         return(
-            <h1>Create page</h1>
+            <Fragment>
+                <h1>Create page</h1>
+                <form>
+                    <input name='title' placeholder='title' onChange={e => this.handleChange(e)}></input>
+                    <input name='description' placeholder='description' onChange={e => this.handleChange(e)}></input>
+                    <input name='content' placeholder='content' onChange={e => this.handleChange(e)}></input>
+                    <input name='category' placeholder='category' onChange={e => this.handleChange(e)}></input>
+                    <button onClick={e => this.handleSubmit(e)}>Crea</button>
+                </form>
+            </Fragment>
         )
     }
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,12 +5,14 @@ import { Posts } from '../../views/PostPage';
 import { SinglePost } from '../single-post/singlePost';
 import { EditPost } from '../edit/Edit';
 import { LoginPage } from '../../views/LoginPage';
+import { CreatePost } from '../create/Create';
 
 export const Main = () => {
     return(
         <Switch>
             <Route exact path='/' component={Home}></Route>
             <Route path='/posts' component={Posts}></Route>
+            <Route path='/create' component={CreatePost}></Route>
             <Route path='/single/:id' component={SinglePost}></Route>
             <Route path='/edit/single/:id' component={EditPost}></Route>
             <Route path='/login' component={LoginPage}></Route>
