80094c0a0a7ab319a96746ba7580fcf5f7171638
ricardo <ricardo.sikic@gmail.com>
2019-10-02 17:28:04 -0300

    ahora se puede subir la categoria solo se cambio el value en options

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -8,35 +8,37 @@ export class CreatePost extends React.Component {
         title: '',
         description: '',
         content: '',
-        selectedFile: null
+        category: '',
+        selectedFile: null,
+        categorias: [],
     }
 
-    // componentDidMount() {
-    //     this.getCategories();
-    // }
+    componentDidMount() {
+        this.getCategories();
+    }
 
-    // getCategories = () => {
-    //     console.log('categoriasss');
-    //     let url = 'http://127.0.0.1:8000/api/';
-    //     const secretKey = localStorage.getItem('key')
+    getCategories = () => {
+        console.log('categoriasss');
+        let url = 'http://127.0.0.1:8000/api/categories/';
+        const secretKey = localStorage.getItem('key')
 
-    //     fetch(url, {
-    //         method: 'GET',
-    //         headers: {
-    //             Authorization: `token ${secretKey}`
-    //         }
-    //     })
-    //     .then(resp => {
-    //         console.log('recibe la info para categorias', resp.status);
-    //         return resp.json();
-    //     })
-    //     .then(info => {
-    //         console.log('recibe las categorias', info);
-    //         this.setState({
-    //             categorias: this.state.categorias.concat(info)
-    //         })
-    //     })
-    // }
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `token ${secretKey}`
+            }
+        })
+        .then(resp => {
+            console.log('recibe la info para categorias', resp.status);
+            return resp.json();
+        })
+        .then(info => {
+            console.log('recibe las categorias', info);
+            this.setState({
+                categorias: this.state.categorias.concat(info)
+            })
+        })
+    }
 
     handleImage = (e) => {
         this.setState({
@@ -62,6 +64,15 @@ export class CreatePost extends React.Component {
         })
     }
 
+    handleCategory = (e) => {
+    
+        this.setState({
+            category: e.target 
+        })
+
+        console.log(this.state.category.value)
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         console.log('posted ', this.state.selectedFile);
@@ -70,6 +81,7 @@ export class CreatePost extends React.Component {
         formData.append('title', this.state.title.value)
         formData.append('description', this.state.description.value)
         formData.append('content', this.state.content.value)
+        formData.append('category', this.state.category.value)
         
         
         const secretKey = localStorage.getItem('key');
@@ -88,6 +100,13 @@ export class CreatePost extends React.Component {
 
 
     render() {
+
+        const cat = this.state.categorias.map((categoria, id) => {
+            return(
+                <option key={id} value={categoria.id}>{categoria.category_name}</option>
+            )
+        })
+
         return(
             <Fragment>
                 <h1>Create page</h1>
@@ -95,7 +114,7 @@ export class CreatePost extends React.Component {
                     <input name='title' placeholder='title' onChange={e => this.handleTitle(e)}></input>
                     <input name='description' placeholder='description' onChange={e => this.handleDescription(e)}></input>
                     <input name='content' placeholder='content' onChange={e => this.handleContent(e)}></input>
-                    {/* <input name='category' placeholder='category' onChange={e => this.handleChange(e)}></input> */}
+                    <select name='category' onChange={e => this.handleCategory(e)}>{cat}</select>
                     <input name='photo' type="file" onChange={e => this.handleImage(e)}/>
                     <button onClick={e => this.handleSubmit(e)}>Crea</button>
                 </form>
